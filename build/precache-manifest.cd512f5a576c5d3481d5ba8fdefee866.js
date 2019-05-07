d1b31c6394d5ac87630dabe5ab0898fd26e72997
leoandry <48559052+leoandry@users.noreply.github.com>
2019-05-07 11:36:34 +0700

    Add files via upload

---
+++ build/precache-manifest.cd512f5a576c5d3481d5ba8fdefee866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d524350975919bbc6ff87c69bf1de03d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e760a0f0332db2de5009",
+    "url": "/static/css/2.3b9b3aa3.chunk.css"
+  },
+  {
+    "revision": "e9c7fa1ca8e620904a09",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "e760a0f0332db2de5009",
+    "url": "/static/js/2.39fa334f.chunk.js"
+  },
+  {
+    "revision": "e9c7fa1ca8e620904a09",
+    "url": "/static/js/main.ba71ab32.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
